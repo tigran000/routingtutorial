044267bfe238641c2c232ce01bdb2b703de3d43d
Tigran <tigranharutynyan59@gmailcom>
2019-03-20 15:00:46 +0400

    adding context

--- src/App.js
+++ src/App.js
@@ -6,43 +6,48 @@ const About = () => <h1> About</h1>
 
 const Contact = () => <h1>Contact</h1>
 
-class App extends Component {
+const RoutingContext = React.createContext()
+
+class Router extends Component {
   state = {
     location: window.location
   }
   render() {
-    const { pathname } = this.state.location
-    let Child
-    switch (pathname) {
-      case "/about":
-        Child = About
-        break
-      case "/home":
-        Child = Home
-        break
-      case "/contact":
-        Child = Contact
-        break
-      default:
-        Child = Home
-    }
+    const { location } = this.state
+    return <RoutingContext.Provider {...this.props} value={{ location }} />
+  }
+}
+
+const Route = ({ path, component: Component }) => (
+  <RoutingContext.Consumer>
+    {({ location }) => (location.pathname === path ? <Component /> : null)}
+  </RoutingContext.Consumer>
+)
+
+class App extends Component {
+  render() {
     return (
-      <>
-        <nav>
-          <ul>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <Child />
-      </>
+      <Router>
+        <>
+          <nav>
+            <ul>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/home">Home</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </>
+      </Router>
     )
   }
 }
